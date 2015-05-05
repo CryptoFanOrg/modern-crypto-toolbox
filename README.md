adb00830412e7d1eadff3c2a9083e4b2e724db36
byronhe <lh2008999@gmail.com>
2015-05-05 04:43:35 -0400

    1.add build_openssl.sh

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
 # modern99
-modern99是一个安全，易用，现代，高性能的密码学组件库
+modern99是一个开箱即用，安全，易用，现代，高性能的密码学组件封装库
 
 鉴于：
 
-1.  密码学是知识背景要求极高，几乎纯数学的一个应用领域，
-2.  密码学通常是高度危险的核心代码，不正确不严密的使用会导致致命的安全危险
-3.  密码学近20年进展巨大，大量旧算法被破解被淘汰，一些现代优秀算法得到学界认可,但是大多数程序员眼界有限，无力跟进这些进展
-4.  大多数程序员对密码学是无知的，而且并不知道自己不知道什么，完全没有能力鉴别crypto算法的优劣，
-5.  大多数密码学库的接口又充满的晦涩的crypto术语(例如openssl)，让大多数程序员敬而远之
+1.  大多数程序员对密码学是无知的，而且并不知道自己不知道什么，完全没有能力鉴别crypto算法的优劣
+2.  密码学是知识背景要求极高，通常是高度危险的核心代码，不正确不严密的使用会导致致命的安全危险
+4.  密码学近20年进展巨大，大量旧算法被破解被淘汰，一些现代优秀算法得到学界认可,但是大多数程序员眼界有限，无力跟进这些进展
+5.  openssl等密码学库的接口又充满的晦涩的术语(例如openssl)，对大多数程序员形成误导
 
-在实际项目中，见到了太多设计缺陷，不安全，过时，危险的设计，这些密码学系统，大多只能打70分(及格，但是离业界高水平相距甚远)
-所以我们需要一个90分的crypto组件库，做到安全，易用，现代，高性能，封装现代密码学的best practice，提升实际项目中密码学的应用水平
+在很多开源项目中，见到了太多设计缺陷，不安全，过时，危险的设计，这些密码学系统，大多只能打70分，及格，但是离业界高水平相距甚远，尤其是在
+中文互联网社区，技术滞后严重，
+所以，我们需要一个99分的密码学组件库，封装现代密码学的best practice
 
 
 modern99分为2部分：
 1. 高层filter, box组件，您所遇到的开发问题，90%都已经被这些组件解决，并且内置best practice，请直接使用
 2. 底层密码学算法，如果filter,box不能满足您，请尝试底层原语，底层原语有大量高度危险的细节，请认真阅读注释/文档
+3. 编译，部署，配置方面的best practice脚本
 
 
 ### 1.  filter,box
-filter,box，作为一个黑盒来用就行，
-如果这个filter/box没有挠到您的痒处，没有让您爽到，那是我的问题，请联系我！
 
+##### 1.  对称传输filter（提供保密+完整性+压缩），
+大多数程序员需要的就是这个对称传输filter
 
-##### 1.  对称传输filter（提供保密+完整性+压缩，保证高性能），
-
-SymmetricFilter.h
+symmetric\_filter.h
 
 zlib+aes-128-gcm
 
@@ -35,70 +34,73 @@ zlib+chacha20-poly1305
 
 ##### 2.  认证密钥协商，提供PFS保证
 
-KeyExchageFilter.h
-ECDSA + ECDH，
+keyexchage\_filter.h
+
+ECDSA + ECDH
+
 rsa-2048
+
 ED25519 + Curve25519
+
+PRF，密码拓展函数
 
 ##### 3.  混合加密box，电子信封，有对方的公钥加密，只有对方才能看到明文(没有PFS保证)
 
-DigitalEnvelope.h
+digital\_envelope.h
+
 rsa-2048 + aes-gcm-128 + hmac-sha256 混合加密，encrypt then MAC
+
 rsa-2048 + aes-cbc-128 + hmac-sha256 混合加密, encrypt then MAC
+
 ECDH-P256 + chacha20 + poly1305 混合加密，encrypt then MAC
+
 Curve25519 + chacha20 + poly1305 混合加密，encrypt then MAC
 
 
 ##### 4.  TLS封装，提供TLS best practice
 
-TLSFilter.h
+tls\_filter.h
 
 利用 BIO\_s\_mem 来做tls异步编程： 
 <http://funcptr.net/2012/04/08/openssl-as-a-filter-(or-non-blocking-openssl)/>
       
-##### 5. 保险箱box，用一个密码存储，用一个密码打开，编辑
+##### 5. 密码保险存储box，用一个密码存储，用一个密码打开，编辑
 
-StoreBox.h
+store\_box.h
+
 scrypt + aes-128-gcm
 
 ### 2. 底层密码学算法
 
-SymmetricCipher.h
-    aes-gcm-128
-    aes-cbc-128
-    chacha20-poly1305
-
-MAC.h
-    hmac-sha256
-    hmac-sha512
-
-DigitalSignature.h
+digital\_signature.h:
     RSA sign/verify
     ECDSA sign/verify
     ED25519 sign/verify
 
-KeyExchange.h
+key\_exchange.h:
     RSA encrypt/decrypt
     ECDH
     Curve25519
 
-PasswordHash.h
+password\_hash.h:
     bcrypt
     pbkdf2
     scrypt
     PRF_sha256
 
-Hash.h
+hash.h:
     sha256
     sha512
     sha1/md5
     siphash
 
-CryptoUtil.h
+crypto\_util.h:
     init()
     mem_cmp_constant_time()
     random
 
+### 3.deployment build scripts
+    build_openssl.sh 这个脚本编译一个安全，高性能的openssl版本
 
 ### 4.internal
 
---
+++ build_openssl.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+
+#openssl优化编译
+
+#查看所有编译选项：
+#grep -r '^#if.*OPENSSL_NO' crypto/ ssl/ | grep -o 'OPENSSL_NO_[a-zA-Z0-9_]*' | sort -u | sed 's/OPENSSL_//' | tr '[A-Z_]' '[a-z-]'
+
+#ARCH的选择是看Configure里面的优化选项，选择优化选项相对更合理的，
+#32 bit linux-elf比较合适，比较糟糕的如 linux-generic32 就关掉了汇编优化
+#64 bit linux-x86_64 比较合适，比较糟糕的如 linux-generic64，也是关掉了汇编优化
+
+#逐选项解释：
+#no-shared               \ 不需要.so，只需要.a
+#threads                 \ 必须支持thread，初始化的时候，需要调用CRYPTO_set_locking_callback
+#no-zlib                 \ 压缩，不需要，有CRIME攻击
+#no-bf                   \ blowfish,废弃
+#no-buf-freelists        \ 不需要，和HeartBleed攻击有关
+#no-camellia             \ camellia，用不到
+#no-cast                 \ cast，废弃
+#no-comp                 \ comp，压缩，用不到，有CRIME攻击，必须关闭
+#no-dtls1                \ dtls，用不到
+#no-decc-init            \ 用不到
+#no-deprecated           \ 去掉一些废弃的api
+#no-dsa                  \ dsa ，用不到
+#no-gmp                  \ gmp，用不到
+#no-gost                 \ gost，淘汰
+#no-heartbeats           \ heartbeats, 用不到，HeartBleed攻击
+#no-idea                 \ idea，淘汰
+#no-jpake                \ jpake，用不到
+#no-krb5                 \ kerberos认证，用不到
+#no-libunbound           \ 用libunbound做dns resolve，用不到
+#no-md2                  \ md2，淘汰
+#no-md4                  \ md4，淘汰
+#no-mdc2                 \ mdc2，淘汰
+#no-psk                  \ psk，用不到
+#no-rc2                  \ rc2，淘汰
+#no-rc5                  \ rc5，淘汰
+#no-sctp                 \ sctp，用不到
+#no-seed                 \ seed，用不到
+#no-sha0                 \ sha0，淘汰
+#no-srp                  \ srp，用不到
+#no-srtp                 \ srtp，用不到
+#no-ssl2                 \ ssl2 必须禁用， POODLE攻击
+#no-ssl3                 \ ssl3 必须禁用， POODLE攻击
+#no-ssl3-method          \ ssl3 必须禁用， POODLE攻击
+#no-unit-test            \
+
+
+function build(){
+
+wget -c https://www.openssl.org/source/$PKG.tar.gz
+
+#校验一下sha1sum
+RIGHT_SHA1=$(curl -k https://www.openssl.org/source/$PKG.tar.gz.sha1)
+REAL_SHA1=$(sha1sum openssl-1.0.2a.tar.gz   | cut  -f 1 -d ' ' ) 
+if [ "$REAL_SHA1" !=  "$RIGHT_SHA1" ] ;  then
+    echo "sha1sum check failed!"
+    exit;
+fi
+
+rm -rf $PREFIX
+mkdir -p $PREFIX
+
+rm -rf $PKG
+mkdir -p $PKG
+tar xf $PKG.tar.gz
+cd $PKG
+
+
+make dclean
+
+./Configure                 \
+    --prefix=$PREFIX        \
+    $ARCH                   \
+    no-shared               \
+    threads                 \
+    no-deprecated           \
+    no-dynamic-engine       \
+    no-zlib                 \
+    no-bf                   \
+    no-buf-freelists        \
+    no-cast                 \
+    no-comp                 \
+    no-dtls1                \
+    no-decc-init            \
+    no-dsa                  \
+    no-gmp                  \
+    no-gost                 \
+    no-heartbeats           \
+    no-idea                 \
+    no-jpake                \
+    no-krb5                 \
+    no-libunbound           \
+    no-md2                  \
+    no-md4                  \
+    no-mdc2                 \
+    no-psk                  \
+    no-rc2                  \
+    no-rc5                  \
+    no-sctp                 \
+    no-sha0                 \
+    no-srp                  \
+    no-srtp                 \
+    no-ssl2                 \
+    no-ssl3                 \
+    no-ssl3-method          \
+    no-unit-test            
+
+make depend
+make
+make install -j8
+
+cd ..
+rm -rf $PKG 
+}
+
+
+
+
+PKG=openssl-1.0.2a
+
+(ARCH="linux-x86_64  enable-ec_nistp_64_gcc_128"
+PREFIX=$(pwd)/${PKG}_64_build
+build
+)
+
+(ARCH=linux-elf
+PREFIX=$(pwd)/${PKG}_32_build
+#need: sudo yum install libstdc++-devel.i686
+export CC="gcc -m32"
+build
+)
+
+wait
+rm -rf $PKG 
